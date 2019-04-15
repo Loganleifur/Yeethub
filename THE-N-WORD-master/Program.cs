cf260f306ae993fc174a2ba306817e5320ab7e51
Logan <LoganLR@outlook.dk>
2019-04-15 12:56:00 +0200

    Added the SANS
    Megalovania

---
+++ THE-N-WORD-master/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sans_dating_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //THIS IS A SIN, WE ARE SO SORRY
+            //I'LL FUCKING DO IT AGAIN
+            //Program starts
+            Console.WriteLine("*********************************");
+            Console.WriteLine("*                               *");
+            Console.WriteLine("*   -------------------------   *");
+            Console.WriteLine("*   SANS UNDERTALE DATING SIM   *");
+            Console.WriteLine("*   -------------------------   *");
+            Console.WriteLine("*                               *");
+            Console.WriteLine("*********************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("- START GAME? 'y'");
+            Console.WriteLine("");
+            Console.Write("- TO EXIT THE GAME, WRITE 'EXIT' ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            bool datruth = true;
+
+            string input;
+            input = Console.ReadLine();
+
+
+                if (input == "y")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("*You wake up one morning and look at your alarm clock*");
+                    Console.ReadKey();
+                    Console.WriteLine("*You're gonna be late for school!*");
+                    Console.ReadKey();
+                    Console.WriteLine("*In a swift motion, you put on your ahegao sweatshirt*");
+                    Console.ReadKey();
+                    Console.WriteLine("*and stuff a piece of toast into your mouth whilst running out the door*");
+                    Console.ReadKey();
+                    Console.WriteLine(" 'I can't be late on my first day of school!' ");
+                    Console.ReadKey();
+                    Console.WriteLine("*without thinking, you run around a corner*");
+                    Console.ReadKey();
+                    Console.WriteLine("*CRASH*");
+                    Console.ReadKey();
+                    Console.WriteLine(" Unknown: 'Ow, what the hell?!' ");
+                    Console.ReadKey();
+                    Console.WriteLine(" 'Hey are you ok?' ");
+                    Console.ReadKey();
+                    Console.WriteLine("*You come back to your senses and look up at a pale stranger*");
+                    Console.ReadKey();
+                    Console.WriteLine("*It's... it's SANS!*");
+                    Console.ReadKey();
+                    Console.WriteLine(" 'You ok? you hit your head pretty hard' ");
+                    Console.ReadKey();
+                    Console.WriteLine("");
+                    Console.WriteLine("Choose a response:");
+                    Console.WriteLine("");
+                    Console.WriteLine(" 1 - 'Watch where you're walking nigga' ");
+                    Console.WriteLine("");
+                    Console.WriteLine(" 2 - 'Oh..y-yeah, i'm fine' ");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+
+                    input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Sans: 'that was uncalled for, i'm just trying to help' ");
+                    Console.WriteLine("");
+                    Console.WriteLine(@"  ________    _____      _____  ___________ ____________   _________________________ ");
+                    Console.WriteLine(@" /  _____/   /  _  \    /     \ \_   _____/ \_____  \   \ /   /\_   _____/\______   \");
+                    Console.WriteLine(@"/   \  ___  /  /_\  \  /  \ /  \ |    __)_   /   |   \   Y   /  |    __)_  |       _/");
+                    Console.WriteLine(@"\    \_\  \/    |    \/    Y    \|        \ /    |    \     /   |        \ |    |   \");
+                    Console.WriteLine(@" \______  /\____|__  /\____|__  /_______  / \_______  /\___/   /_______  / |____|_  /");
+                    Console.WriteLine(@"        \/         \/         \/        \/          \/                 \/         \/ ");
+                    Console.WriteLine("");
+                    Console.WriteLine("Bad ending: called Sans a nigger");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Sans: 'Okay, well, just incase, i got an idea of how we can turn that frown upside down' ");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'Let's make one thing perfectly clear' ");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonna say some words up here' ");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I haven't had the chance to say before' ");
+                    Console.ReadKey();
+                    Console.WriteLine("You ask sans where he was going with this, but he ignores you");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonna say homie'");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonna say bro' ");
+                    Console.ReadKey();
+                    Console.WriteLine("You look in wonder at the rapping skeleton, it fills you with determination");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonn say MY MAN!' ");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonna say fo sho' ");
+                    Console.ReadKey();
+                    Console.WriteLine("You start to feel the rhythm");
+                    Console.ReadKey();
+                    Console.WriteLine("Sans: 'I'm gonna say the N-word!' ");
+                    Console.ReadKey();
+                    Console.WriteLine("You stab Sans repeatedly");
+                    Console.ReadKey();
+                    Console.WriteLine(@"  ________    _____      _____  ___________ ____________   _________________________ ");
+                    Console.WriteLine(@" /  _____/   /  _  \    /     \ \_   _____/ \_____  \   \ /   /\_   _____/\______   \");
+                    Console.WriteLine(@"/   \  ___  /  /_\  \  /  \ /  \ |    __)_   /   |   \   Y   /  |    __)_  |       _/");
+                    Console.WriteLine(@"\    \_\  \/    |    \/    Y    \|        \ /    |    \     /   |        \ |    |   \");
+                    Console.WriteLine(@" \______  /\____|__  /\____|__  /_______  / \_______  /\___/   /_______  / |____|_  /");
+                    Console.WriteLine(@"        \/         \/         \/        \/          \/                 \/         \/ ");
+                    Console.ReadKey();
+                    Console.WriteLine("");
+                    Console.WriteLine("Bad ending: You killed a dead person who just wanted to be black");
+                    Console.WriteLine("");
+                    
+                    //Sans is not pleased.
+                    
+            Console.WriteLine(@"░░░░░░░░░░░░░░░░██████████████████");
+            Console.WriteLine(@"░░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░████");
+            Console.WriteLine(@"░░░░░░░░░░██░░░░░░░░░░░░░░░░░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░░░██░░░░░░░░░░░░░░░░░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░██░░░░░░░░░░░░░░░░░░░░██████░░░░██");
+            Console.WriteLine(@"░░░░░░░░██░░░░░░░░░░░░░░░░░░░░██████░░░░██");
+            Console.WriteLine(@"░░░░░░░░██░░░░██████░░░░██░░░░██████░░░░██");
+            Console.WriteLine(@"░░░░░░░░░░██░░░░░░░░░░██████░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░████░░██░░░░░░░░░░░░░░░░░░██░░████");
+            Console.WriteLine(@"░░░░░░░░██░░░░██████████████████████░░░░██");
+            Console.WriteLine(@"░░░░░░░░██░░░░░░██░░██░░██░░██░░██░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░░░████░░░░██████████████░░░░████");
+            Console.WriteLine(@"░░░░░░░░██████████░░░░░░░░░░░░░░██████████");
+            Console.WriteLine(@"░░░░░░██░░██████████████████████████████░░██");
+            Console.WriteLine(@"░░░░████░░██░░░░██░░░░░░██░░░░░░██░░░░██░░████");
+            Console.WriteLine(@"░░░░██░░░░░░██░░░░██████░░██████░░░░██░░░░░░██");
+            Console.WriteLine(@"░░██░░░░████░░██████░░░░██░░░░██████░░████░░░░██");
+            Console.WriteLine(@"░░██░░░░░░░░██░░░░██░░░░░░░░░░██░░░░██░░░░░░░░██");
+            Console.WriteLine(@"░░██░░░░░░░░░░██░░██░░░░░░░░░░██░░██░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░██░░░░░░██░░░░████░░░░░░████░░░░██░░░░░░██");
+            Console.WriteLine(@"░░░░░░████░░██░░░░██░░░░░░░░░░██░░░░██░░████");
+            Console.WriteLine(@"░░░░░░░░██████░░░░██████████████░░░░██████");
+            Console.WriteLine(@"░░░░░░░░░░████░░░░██████████████░░░░████");
+            Console.WriteLine(@"░░░░░░░░██████████████████████████████████");
+            Console.WriteLine(@"░░░░░░░░████████████████░░████████████████");
+            Console.WriteLine(@"░░░░░░░░░░████████████░░░░░░████████████");
+            Console.WriteLine(@"░░░░░░██████░░░░░░░░██░░░░░░██░░░░░░░░██████");
+            Console.WriteLine(@"░░░░░░██░░░░░░░░░░████░░░░░░████░░░░░░░░░░██");
+            Console.WriteLine(@"░░░░░░░░██████████░░░░░░░░░░░░░░██████████");
+                }
+
+            }
+                else if (input == "exit")
+                {
+                    datruth = false;
+                }
+            }
+        }
+    }
---
+++ THE-N-WORD-master/README.md
@@ -0,0 +1,3 @@
+Ting og sager
+Sans visual novel i guess
+Honestly ved ikke hvis vi får mere end 15 linjer
